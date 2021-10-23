a546754347e31015097dfbecc05d085e16e4d35e
Nima <meghdadi.nima@gmail.com>
2021-10-23 12:22:43 +0330

    fix login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,29 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroupDirective, NgForm, Validators} from '@angular/forms';
-import {ErrorStateMatcher} from '@angular/material/core';
+import {
+  FormControl,
+  FormGroupDirective,
+  NgForm,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { Router } from '@angular/router';
+import { ParamsHandler } from 'src/app/core/params-handler';
+import { GlobalService } from 'src/app/core/services/global.service';
+import { ApiRequest } from 'src/app/core/services/request.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-
 export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  login: boolean;
 
-  constructor() { }
-  ngOnInit(): void {
+  constructor(private router: Router, private gs: GlobalService) {}
+  ngOnInit(): void {}
+
+  usernameFormController = new FormControl('', [Validators.required]);
+
+  //  matcher = new MyErrorStateMatcher();
+
+  loginReq() {
+    let params = new ParamsHandler();
+    params.addParam('username', this.username);
+    params.addParam('password', this.password);
+    this.isUserAndPassValid(params);
   }
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
 
-  matcher = new MyErrorStateMatcher();
+  isUserAndPassValid(param: ParamsHandler) {
+    ApiRequest('POST')
+      .controller('auth')
+      .action('login')
+      .setBody(param)
+      .call(this.gs)
+      .subscribe((resp) => {
+        console.log(resp);
+        if (resp.accessToken) this.router.navigate(['search/home']);
+      });
+  }
 }
 
-export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-    const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
-  }
-}
+// export class MyErrorStateMatcher implements ErrorStateMatcher {
+//   isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+//     const isSubmitted = form && form.submitted;
+//     return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+//   }
+
+// }
