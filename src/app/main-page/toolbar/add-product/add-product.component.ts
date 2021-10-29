abd6c741666a73ab18336c8aec2fb5d075d61dc2
Nima <meghdadi.nima@gmail.com>
2021-10-29 23:44:59 +0330

    fix add product

--- src/app/main-page/toolbar/add-product/add-product.component.ts
+++ src/app/main-page/toolbar/add-product/add-product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, Validators } from '@angular/forms';
-
+import { Router } from '@angular/router';
+import { ParamsHandler } from 'src/app/core/params-handler';
+import { GlobalService } from 'src/app/core/services/global.service';
+import { ApiRequest } from 'src/app/core/services/request.service';
 interface Animal {
   name: string;
   sound: string;
@@ -17,9 +20,10 @@ export class AddProductComponent implements OnInit {
     category: ['', Validators.required],
     price: ['', Validators.required],
     amount: ['1', Validators.required],
+    describtion: [''],
   });
   errors = errorMessages;
-  constructor(private formbuilder: FormBuilder) {}
+  constructor(private formbuilder: FormBuilder, private gs: GlobalService) {}
   ngOnInit(): void {}
 
   category = new FormControl();
@@ -36,10 +40,34 @@ export class AddProductComponent implements OnInit {
     'smartWatch',
     'homeDesign',
   ];
+  addProductReq(){
+    let params = new ParamsHandler();
+    params.addParam('name', this.addProductForm.get("name").value);
+    params.addParam('category', this.addProductForm.get("category").value);
+    params.addParam('price', this.addProductForm.get("price").value);
+    params.addParam('amount', this.addProductForm.get("amount").value);
+    params.addParam('describtion', this.addProductForm.get("describtion").value);
+    this.insertProduct(params);
+
+  }
+  insertProduct(param: ParamsHandler) {
+    ApiRequest('POST')
+      .controller('product')
+      .action('addProduct')
+      .setBody(param)
+      .call(this.gs)
+      .subscribe((resp) => {
+        console.log(resp);
+      });
+  }
+
+
+
+
 }
 export const errorMessages: { [key: string]: string } = {
   name: 'write Name',
   category: 'Please choose at least ONE category',
   price: 'Write IT',
-  amount: '1 2 ...'
+  amount: '1 2 ...',
 };
